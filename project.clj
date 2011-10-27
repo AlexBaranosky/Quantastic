e3a0bf77072815701d11a39044470a14fc4193b0
Alex Baranosky <alexander.baranosky@gmail.com>
2011-10-26 23:18:13 -0400

    starting to do some screen scraping

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject quant "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject quant "0.0.1"
+  :description "quant awesomeness"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojars.kyleburton/clj-xpath "1.0.10"]
+                 [joda-time "2.0"]]
+  :dev-dependencies [[midje "1.2-alpha4"]])
--- test/quant/test/core.clj
+++ test/quant/test/core.clj
@@ -1,6 +1,22 @@
 (ns quant.test.core
-  (:use [quant.core])
-  (:use [clojure.test]))
+  (:use [quant.core]  
+        [org.clojars.kyleburton.clj-xpath]
+        [midje.sweet]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn extract [html]
+
+  ;; TODO : tidy up the html, or just use enlive instead yeah lets do that
+
+  (let [trs ($x "//tr[contains(@class, 'dataSmall')]" html)
+        doit (fn [td1 td2 td3 td4 td5 td6 & _ :as tr]
+                      {:ric nil ;;(-> td5 )
+                       :direction (first td4) 
+                       :transaction-date nil })]
+    (prn (map doit trs))))                   
+
+(def sample-page (slurp "./test/quant/test/sample-page.html"))
+
+(fact "pulls list of transaction info from html string"
+  (extract sample-page) => [ { :ric "VOD.L"
+                               :direction :buy
+                               :transaction-date [12 31 2000] }])
