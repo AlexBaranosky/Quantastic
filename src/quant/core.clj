92fa3f6bf9d06f067de5b9951b74d7f9f746580a
Alex Baranosky <alexander.baranosky@gmail.com>
2011-10-27 03:36:01 -0400

    moved application code out of test directory into src/core.clj

--- src/quant/core.clj
+++ src/quant/core.clj
@@ -1,1 +1,21 @@
-(ns quant.core)
+(ns quant.core
+  (:use [apricot-soup :only ($ s-expressions)]))
+
+(defn- third [coll]
+  (nth coll 2))
+
+(defn ric-from-params [param-string]
+  (second (re-matches #"^.*\?symbol=([^&]+).*$" param-string)))
+
+(defn extract-via-scrape
+  "Extracts trade-idea data from an html source"
+  [html]
+  (let [trs (map third ($ html "tr" (s-expressions)))
+        tr->trade-idea (fn [[td1 td2 _ td4 _ td6]]
+                          {
+                            :ric (ric-from-params (-> td2 third first third first second :href))
+                            :direction (third td4)
+                            :amount (third td6)
+                            :transaction-date (third td1)
+                          } )]
+    (map tr->trade-idea trs)))
--- test/quant/test/core.clj
+++ test/quant/test/core.clj
@@ -1,32 +1,9 @@
 (ns quant.test.core
-  (:use [quant.core]
-        [apricot-soup]
+  (:use [quant.core :only (ric-from-params extract-via-scrape)]
         [midje.sweet])
   (:import [org.joda.time DateMidnight]
            [org.w3c.tidy Tidy]
            [java.io StringWriter StringReader]))
-
-(defn ric-from-params [param-string]
-  (second (re-matches #"^.*\?symbol=([^&]+).*$" param-string)))
-
-(fact "pulls RIC out of link params"
-  (ric-from-params "?symbol=GOOG.O&name=ARORA+NIKESH") => "GOOG.O")
-
-(defn third [coll]
-  (nth coll 2))
-
-(defn extract-via-scrape
-  "Extracts trade-idea data from an html source"
-  [html]
-  (let [trs (map third ($ html "tr" (s-expressions)))
-        tr->trade-idea (fn [[td1 td2 _ td4 _ td6]]
-                          {
-                            :ric (ric-from-params (-> td2 third first third first second :href))
-                            :direction (third td4)
-                            :amount (third td6)
-                            :transaction-date (third td1)
-                          } )]
-    (map tr->trade-idea trs)))
 
 (def sample-page (slurp "./test/quant/test/sample-page.html"))
 
@@ -55,6 +32,9 @@
       <td>$598.75</td>
     </tr>
 	<table>")
+
+(fact "pulls RIC out of link params"
+  (ric-from-params "?symbol=GOOG.O&name=ARORA+NIKESH") => "GOOG.O")
 
 (fact "pulls list of transaction info from html string"
   (extract-via-scrape sample-trs) => [ { :ric "GOOG.O"
