f8a365fcaeaf0c30cf740a1fac94e68c8af79755
Alex Baranosky <alexander.baranosky@gmail.com>
2011-10-27 04:08:24 -0400

    oops, killed an unused piece of testing data

--- test/quant/test/core.clj
+++ test/quant/test/core.clj
@@ -8,32 +8,6 @@
 
 (def sample-page (slurp "./test/quant/test/sample-page.html"))
 
-(def sample-trs
-  "<table>
-    <tr class=\"dataSmall stripe\">
-      <td>14 Oct 2011</td>
-      <td>
-        <h2 class=\"officers\">
-          <a href=\"?symbol=GOOG.O&name=ARORA+NIKESH\">ARORA NIKESH</a>
-        </h2>
-      </td>
-      <td>Officer</td>
-      <td> Buy</td>
-      <td>
-        <a href=\"insiderTradingDetails?symbol=GOOG.O&accession=111146730&trade_no=4\">934</a>
-      </td>
-      <td>$0.00</td>
-    </tr>
-    <tr class=\"dataSmall \">
-      <td>13 Oct 2011</td>
-      <td><h2 class=\"officers\"><a href=\"?symbol=GOOG.O&name=ARORA+NIKESH\">SECOND OFFICER</a></h2></td>
-      <td>Officer</td>
-      <td> Sell</td>
-      <td><a href=\"insiderTradingDetails?symbol=GOOG.O&accession=111146730&trade_no=1\">2,266</a></td>
-      <td>$598.75</td>
-    </tr>
-	<table>")
-
 (fact "pulls RIC out of link params"
   (ric-from-params "?symbol=GOOG.O&name=ARORA+NIKESH") => "GOOG.O")
 
